e39d7b57e92f046adadc6d5cb90b625e67ed4306
vsevex <vsevex@gmail.com>
2024-06-21 02:31:56 +0400

    add `ground` sample

---
+++ src/components/Ground.jsx
@@ -0,0 +1,17 @@
+const Ground = () => {
+  return (
+    <div
+      style={{
+        position: "absolute",
+        bottom: "0",
+        left: "0",
+        width: "100%",
+        height: "60px",
+        backgroundColor: "#DEB887",
+        borderTop: "5px solid #8B4513",
+      }}
+    ></div>
+  );
+};
+
+export default Ground;
