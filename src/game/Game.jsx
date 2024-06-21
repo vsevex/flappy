f95a3a134c9eb241f83a15dc8a0b0523a255272d
vsevex <vsevex@gmail.com>
2024-06-21 17:14:21 +0400

    basic pipe rendering

--- src/game/Game.jsx
+++ src/game/Game.jsx
@@ -1,18 +1,48 @@
 import background from "../../public/assets/background.png";
 import { useState, useEffect, useRef } from "react";
 import Ground from "../components/Ground";
+import Pipe from "../components/Pipe";
 
 const Game = () => {
   const [birdY, setBirdY] = useState(200);
   const [birdVelocity, setBirdVelocity] = useState(0);
   const [isRunning, setIsRunning] = useState(false);
+  const [pipes, setPipes] = useState([]);
   const birdRef = useRef();
+
+  const pipeWidth = 60;
+  const pipeGap = 150; /// Gap between top and bottom pipes
+
+  useEffect(() => {
+    if (isRunning) {
+      const pipeInterval = setInterval(() => {
+        setPipes((pipes) => [
+          ...pipes,
+          {
+            left: window.innerWidth,
+            topHeight:
+              Math.random() * (window.innerHeight - pipeGap - 200) + 50,
+            bottomHeight:
+              Math.random() * (window.innerHeight - pipeGap - 200) + 50,
+          },
+        ]);
+      }, 2000);
+
+      return () => clearInterval(pipeInterval);
+    }
+  }, [isRunning]);
 
   useEffect(() => {
     if (isRunning) {
       const gameInterval = setInterval(() => {
         setBirdY((y) => Math.min(y + birdVelocity, window.innerHeight - 100));
         setBirdVelocity((v) => v + 1);
+
+        setPipes((pipes) =>
+          pipes
+            .map((pipe) => ({ ...pipe, left: pipe.left - 5 }))
+            .filter((pipe) => pipe.left + pipeWidth > 0)
+        );
       }, 30);
 
       return () => clearInterval(gameInterval);
@@ -38,6 +68,7 @@ const Game = () => {
     setIsRunning(true);
     setBirdY(200);
     setBirdVelocity(0);
+    setPipes([]);
   };
 
   return (
@@ -49,7 +80,7 @@ const Game = () => {
         backgroundColor: "#121212",
         backgroundImage: `url(${background})`,
         backgroundSize: "cover",
-        // backgroundPosition: "center",
+        backgroundPosition: "center",
         backgroundRepeat: "no-repeat",
         position: "relative",
       }}
@@ -66,6 +97,14 @@ const Game = () => {
           left: "50px",
         }}
       ></div>
+      {pipes.map((pipe, index) => (
+        <Pipe
+          key={index}
+          topHeight={pipe.topHeight}
+          bottomHeight={window.innerHeight - pipeGap - pipe.topHeight}
+          left={pipe.left}
+        ></Pipe>
+      ))}
       <Ground />
     </div>
   );
